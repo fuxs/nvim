9942aa1a682ebd16e2d4307e6e326091e6cd593b
Michael Bungenstock <bungenstock@google.com>
2026-03-10 18:33:11 +0100

    better resizing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,10 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' 
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-S-h>', '<cmd>wincmd<lt><cr>', { desc = 'Shrink window horizontally' })
+vim.keymap.set('n', '<C-S-j>', '<cmd>wincmd-<cr>', { desc = 'Shrink window vertically' })
+vim.keymap.set('n', '<C-S-k>', '<cmd>wincmd+<cr>', { desc = 'Expand window vertically' })
+vim.keymap.set('n', '<C-S-l>', '<cmd>wincmd><cr>', { desc = 'Expand window horizontally' })
 
 -- save file
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
